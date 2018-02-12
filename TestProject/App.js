e4bcfe8c481944fa74fb99c4ab6158c78e73dfc8
Dora Do <dorado@aes-macbook-air.lan>
2018-02-11 19:31:00 -0800

    push

--- TestProject/App.js
+++ TestProject/App.js
@@ -5,9 +5,12 @@ import { StackNavigator } from 'react-navigation';
 import HomeScreen from './components/HomeScreen';
 import StepScreen from './components/StepScreen';
 import DoneScreen from './components/DoneScreen';
+import NavigationMenu from './components/NavigationMenu';
+
 
 export default StackNavigator({
   Home: { screen: HomeScreen },
   Step: { screen: StepScreen },
-  Done: { screen: DoneScreen }
+  Done: { screen: DoneScreen },
+  NavigationMenu: {screen: NavigationMenu}
 });
